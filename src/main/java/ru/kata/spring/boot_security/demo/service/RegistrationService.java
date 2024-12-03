54ffd065332d60aa2eba2b6982d604fd9261dc7d
IvanPrut <ivanprut@list.ru>
2024-12-03 18:46:18 +0300

    Add user registration service
    A registration service has been created to register users. All registered users have the ROLE_USER role. Administrators are assigned by the super administrator on their page. The super administrator is assigned via the database.

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RegistrationService.java
@@ -0,0 +1,38 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+import java.util.Optional;
+
+
+@Service
+public class RegistrationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RegistrationService(UserRepository userRepository, PasswordEncoder passwordEncoder,
+                               RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+    }
+
+    public void register(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        Optional<Role> role = roleRepository.findByRole("ROLE_USER");
+        if (role.isPresent()) {
+            user.getRoles().add(role.get());
+        } else {
+            System.out.println("Role not found");
+        }
+        userRepository.save(user);
+    }
+}
